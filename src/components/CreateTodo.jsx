2c72e9b4013b99dc8bff339eb350f98e94fee2df
Harsha Anuga <harshaanuga358@gmail.com>
2025-03-20 15:36:04 +0530

    added frontend

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,7 +1,13 @@
 export function CreateTodo(){
     return<div>
-        <input type="text" placeholder="title"></input><br></br>
-        <input type="text" placeholder="title"></input><br></br>
+        <input style={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="title"></input><br></br>
+        <input style={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="title"></input><br></br>
         <button>Add a Todo</button> 
     </div>
 }
